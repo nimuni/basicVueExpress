40b1eefdbb0f35384eab68798592cb65e0a81f16
nimuni <kym9788@gmail.com>
2020-09-29 16:43:28 +0900

    test

--- be/routes/test.js
+++ be/routes/test.js
@@ -1,5 +1,16 @@
-var express = require('express');
-var router = express.Router();
+"use strict"
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+require('dotenv').config()
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Successfully connected to mongodb'))
+  .catch(e => console.error(e));
+
 
 let data = 
 [
